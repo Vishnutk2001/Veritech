4e6cde7b1c6591f92a8efbc1684c0239dc3bba5f
Vishnutk2001 <100752044+Vishnutk2001@users.noreply.github.com>
2024-02-02 23:05:53 +0530

    Add files via upload

---
+++ Task-4/data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: ' falcon and the winter soldier is an American television series created by Michael Waldron for the streaming service Disney+, based on Marvel Comics featuring the character of the same name. ',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'Loki is an American television series created by Michael Waldron for the streaming service Disney+, based on Marvel Comics featuring the character of the same name. ',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'wanda vision is an American television series created by Michael Waldron for the streaming service Disney+, based on Marvel Comics featuring the character of the same name. ',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'raya and the last dragon is an American television series created by Michael Waldron for the streaming service Disney+, based on Marvel Comics featuring the character of the same name. ',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'luca is an American television series created by Michael Waldron for the streaming service Disney+, based on Marvel Comics featuring the character of the same name. ',
+        image: 'images/slider 5.PNG'
+    }
+]
